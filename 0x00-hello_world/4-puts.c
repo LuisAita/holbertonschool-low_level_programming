40979c892830f33310cf3f24427e8d9d12628f1d
LuisAita <titiaita@gmail.com>
2022-02-03 21:18:13 +0000

    First program compiled puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 char puzzle[] = "\"Programming is like building a multilingual puzzle";
 
 puts(puzzle);
+
 return (0);
 }
