f8e0d1844bb3954edefe01240fb7ad8a425c7d91
LuisAita <titiaita@gmail.com>
2022-02-04 19:10:55 +0000

    write function with new library unistd.h

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	char messg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, messg, 59);
+	return (1);
 }
