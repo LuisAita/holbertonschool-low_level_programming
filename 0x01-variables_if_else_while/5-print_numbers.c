c2206f07922e8b7a2ccd701abf69bb7f2aa1e40a
LuisAita <titiaita@gmail.com>
2022-02-05 01:24:59 +0000

    for loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, i;
+
+	n = 0;
+
+	for (i = 0 ;  i < 10 ; i++)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar(10);
+	return (0);
+}
