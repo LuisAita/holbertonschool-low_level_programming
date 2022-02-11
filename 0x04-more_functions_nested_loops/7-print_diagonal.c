59408c626deb3cb3917591811f29039186ddbd74
LuisAita <titiaita@gmail.com>
2022-02-11 05:20:23 +0000

    diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ * print_diagonal: print a diagonal line
+ * @n: input- size of line
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	j = 0;
+
+	while (j < n)
+	{
+		for (i = 0; i <= j; i++)
+		{
+			_putchar(32);
+
+		}
+		_putchar(92);
+		_putchar(10);
+		j++;
+	}
+	_putchar(10);
+
+}
