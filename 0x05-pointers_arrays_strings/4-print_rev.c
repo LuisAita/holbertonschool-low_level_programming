7020383b01d55f14443437a5d09baa823a622f4b
LuisAita <titiaita@gmail.com>
2022-02-15 12:03:27 +0000

    print rev and strlen fuction

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,26 +1,39 @@
 #include "main.h"
 
 /**
- * print_rev - print string in reverse
+ * _strlen - function that get the lenght
  * @s: string passed
  * Return: Your bank account savings
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+	int c = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+		c = i;
+	}
+	return (c);
+}
+
+/**
+ * print_rev - print string in reverse
+ * @s : same
  */
 
 void print_rev(char *s)
 {
 
-	int i = 0;
-	int strlen;
+	int a;
 
-	while (s[i] != '\0')
-	{
-		i++;
-		strlen = i;
-	}
+	a = _strlen(s);
 
-	for  (strlen--; strlen >= 0 ; strlen--)
+	for  (a--; a >= 0 ; a--)
 	{
-		_putchar(s[strlen]);
+		_putchar(s[a]);
 	}
 	_putchar(10);
 }
